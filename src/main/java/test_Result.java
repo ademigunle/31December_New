8c2e28c19bf3c03f9e062722b87ed4c6b68acfbe
Ademigunle <Potters@17>
2020-12-31 17:44:16 +0000

    Committing the project two Github

---
+++ src/main/java/test_Result.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+public class test_Result {
+    @Test
+    public void testResult1(){
+        System.out.println("Running test Result 1");
+    }
+    @Test
+    public void testResult2(){
+        System.out.println("Running test Result 2");
+
+    }
+     @Test
+public void testResult3(){
+    System.out.println("Running test Result 3");
+}
+    @Test
+public void testResult4(){
+
+        System.out.println("Running test Result 4");
+}
+}
